f5fd9ce3dd75786418519f51b8665022cdffb982
Victor_Duarte <v.duarteq@uniandes.edu.co>
2024-05-12 15:24:24 -0500

    BackstopJS

--- cypress-tests/cypress/e2e/pages/pages_list.cy.js
+++
@@ -1,7 +0,0 @@
-context('Error en inicio de sesion por email y contraseña incorrecta.', () => {
-  beforeEach(() => {
-    // Ingresar a la plataforma Ghost
-    cy.visit('http://54.188.89.84/ghost/');
-  })
-
-})
